d7a4efe9f53c94a4a6df5a5576101a10605af7e4
Jesus Mosconi <jechu1.mosconi@gmail.com>
2022-09-02 18:44:31 -0300

    re entrega

--- js/index.js
+++ js/index.js
@@ -2,33 +2,32 @@
 let producto = prompt("Que producto busca? (reja, porton, escalera) (Para salir use ESC)")
 let precio = ""
 let IVA = 1.21
-let seguir = true
- 
-
- while(producto != "ESC"){
 
 
-if (producto === "reja"){
-    precio = 15000
-    console.log("El precio es de: 15000")
-}
-else if(producto === "porton"){
-    precio = 50000
-    console.log("El precio es de: 50000")
-}
-else if(producto === "escalera"){
-    precio = 35000
-    console.log("El precio es de: 35000")
-}
-else{
-    console.warn("No hay stock del producto ingresado.")
-}
+while (producto != "ESC") {
 
-function precioConIva(){
-    console.log("El precio final es de:", precio * IVA)
-}
+    if (producto === "reja") {
+        precio = 15000
+        console.log("El precio es de: 15000")
+    }
+    else if (producto === "porton") {
+        precio = 50000
+        console.log("El precio es de: 50000")
+    }
+    else if (producto === "escalera") {
+        precio = 35000
+        console.log("El precio es de: 35000")
+    }
+    else {
+        console.warn("No hay stock del producto ingresado.")
+        precio = 0
+    }
 
-precioConIva()
 
-producto = prompt("Que producto busca? (reja, porton, escalera) (Para salir use ESC)")
+    function precioConIva() {
+        console.log("El precio final es de:", precio * IVA)
+    }
+
+    precioConIva()
+    producto = prompt("Desea comprar otro producto? (reja, porton, escalera) (Para salir use ESC)")
 } 
